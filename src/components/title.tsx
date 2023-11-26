3fcba1fbb87f6eab0cc40d574aa64ddb56cd0872
Hugh Forte <hugh.forte@gmail.com>
2023-11-26 12:47:20 -0800

    feat: extract title component, add wikipedia link

---
+++ src/components/title.tsx
@@ -0,0 +1,12 @@
+export const Title = () => (
+  <h1 className='title'>
+    a game of life
+    <a
+      href='https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life'
+      target='_blank'
+      rel='noopener noreferrer'
+    >
+      *
+    </a>
+  </h1>
+)
